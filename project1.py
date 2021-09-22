190645edcc86c817e83cead15ddd87629ddaa441
Berlian Isnaini <banunberlian.2020@student.uny.ac.id>
2021-09-23 01:19:08 +0700

    init

---
+++ project1.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+master = tk.TK()
+master.title("First UI Code")
+master.geometry("300x300")
+
+tk.Label(master, text="Nama : ").grid(row=0, column=0)
+
+tk.mainloop()
+python3 project1.py
