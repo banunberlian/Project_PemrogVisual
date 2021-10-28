bf3a6816484ebf1689c1c03fd9ed1c3740a1422f
Berlian Isnaini <banunberlian.2020@student.uny.ac.id>
2021-10-28 10:06:13 +0700

    changes

--- MyProject.py
+++ MyProject.py
@@ -4,4 +4,9 @@ window = tk.Tk()
 window.title('Data Mahasiswa')
 window.geometry('600x400')
 
+tk.Label(window, text="Nama : ").grid(row=0, column=0)
+tk.Label(window, text="Nim  : ").grid(row=1, column=0)
+tk.Label(window, text="Alamat : ").grid(row=2, column=0)
+tk.Label(window, text="Jenis Kelamin : ").grid(row=3, column=0)
+
 window.mainloop()
