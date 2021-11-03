a8f45fcf808a954fa236d78939ac3c7a95719941
Berlian Isnaini <banunberlian.2020@student.uny.ac.id>
2021-11-03 23:12:30 +0700

    changes

--- MyProject.py
+++ MyProject.py
@@ -4,6 +4,16 @@ window = tk.Tk()
 window.title('Data Mahasiswa')
 window.geometry('600x400')
 
+def createNama():
+    nama = inputNama.get()
+    tk.Label(window, text="Nama : " + nama).grid(row=5, column=1)
+    nim = inputNim.get()
+    tk.Label(window, text="Nim: " + nim).grid(row=7, column=1)
+    alamat = inputAlamat.get()
+    tk.Label(window, text="Alamat: " + alamat).grid(row=8, column=1)
+    jeniskelamin = inputJenisKelamin.get()
+    tk.Label(window, text="Jenis Kelamin: " + jeniskelamin).grid(row=9, column=1)
+
 inputNama = tk.Entry(window)
 inputNim = tk.Entry(window)
 inputAlamat = tk.Entry(window)
@@ -19,4 +29,5 @@ inputNim.grid(row=1, column=1)
 inputAlamat.grid(row=2, column=1)
 inputJenisKelamin.grid(row=3, column=1)
 
+tk.Button(window, text="Click", command=createNama).grid(row=4, column=1)
 window.mainloop()
