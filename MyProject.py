a2d995cc35f55c13e800380c1a0b8aa06ea1ef04
Berlian Isnaini <banunberlian.2020@student.uny.ac.id>
2021-11-25 01:42:44 +0700

    changes

--- MyProject.py
+++ MyProject.py
@@ -13,21 +13,26 @@ def create():
     tk.Label(window, text="Alamat: " + alamat).grid(row=8, column=1)
     jeniskelamin = inputJenisKelamin.get()
     tk.Label(window, text="Jenis Kelamin: " + jeniskelamin).grid(row=9, column=1)
+    tanggallahir = inputTanggalLahir.get()
+    
 
 inputNama = tk.Entry(window)
 inputNim = tk.Entry(window)
 inputAlamat = tk.Entry(window)
 inputJenisKelamin = tk.Entry(window)
+inputTanggalLahir = tk.Entry(window)
 
 tk.Label(window, text="Nama : ").grid(row=0, column=0)
 tk.Label(window, text="Nim  : ").grid(row=1, column=0)
 tk.Label(window, text="Alamat : ").grid(row=2, column=0)
 tk.Label(window, text="Jenis Kelamin : ").grid(row=3, column=0)
+tk.Label(window, text="Tanggal Lahir : ").grid(row=4, column=0)
 
 inputNama.grid(row=0, column=1)
 inputNim.grid(row=1, column=1)
 inputAlamat.grid(row=2, column=1)
 inputJenisKelamin.grid(row=3, column=1)
+inputTanggalLahir.grid(row=4, column=1)
 
 tk.Button(window, text="submit", command=create).grid(row=4, column=1)
 window.mainloop()
